f770ae9805b299534d47fc3d9cbbb7ecb20dbbcd
Pratik881 <pratikn484@gmail.com>
2025-01-28 12:32:37 +0545

    Initial commit with all project files

---
+++ Product CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Product_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Data;
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Category> Categories { get; set; }
+}
